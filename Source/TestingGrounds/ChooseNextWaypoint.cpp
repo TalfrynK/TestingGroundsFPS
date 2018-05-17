d1d58d819d2bd2a2412238d57bf401ee03478eb3
NITROTK\avine <aviraj.newatia@gmail.com>
2018-05-17 06:12:14 +0530

    Lecture 227. Switched PatrolRoute from Inheritance to an Actor Component. Tested Working.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,17 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 #include "TestingGrounds.h"
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	auto AIGuard = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIGuard->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	
+
+	//Protecting against no Patrol Points
+		
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	//Protecting against no PatrolRoute Compnent
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+
+	if (PatrolPoints.Num() == 0) {
+		UE_LOG(LogTemp, Warning, TEXT("No Patrol Points Detected!"));
+		return EBTNodeResult::Failed;
+	}
 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
